de6ee1830c3fb0e9638a3e3abf51828c3761606a
vanessazook229-art <vanessa.zook229@students.cmcss.net>
2026-02-20 20:21:54 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,32 @@
+# Convert each variable to the most suitable data type
+# Hint: print and check type if unsure
+# Not all can or should be converted to a number
+
+# a
+print(a)
+print(type(a))
+a = float(a)
+
+# b
+print(b)
+print(type(b))
+
+# c
+print(c)
+print(type(c))
+
+# d
+print(d)
+print(type(d))
+d = int(d)
+
+# e
+print(e)
+print(type(e))
+
+# f
+print(f)
+print(type(f))
+f = float(f)
+
 
